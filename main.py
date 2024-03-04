fd41f63d314ed4797e22b301f2f17b6d0c5f9874
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-03-04 08:32:24 -0600

    Update GUI script path and handle subprocess errors

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-#!/user/bin/env python
 """
 This module runs the main GUI of the application using the subprocess module.
 """
 
+import os
+import sys
 import subprocess
 
-subprocess.run(["python", "Program/Main/GUI.py"], check=True)
+# Check if we're running in a PyInstaller bundle
+if getattr(sys, "frozen", False):
+    application_path = sys._MEIPASS  # pylint: disable=W0212
+else:
+    application_path = os.path.dirname(os.path.abspath(__file__))
+
+gui_script_path = os.path.join(application_path, "Program", "Main", "GUI.py")
+
+try:
+    subprocess.run(["python", gui_script_path], check=True)
+except subprocess.CalledProcessError as e:
+    print(f"An error occurred: {e.output}")
+    input("Press enter to exit.")
